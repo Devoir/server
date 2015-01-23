6c8ddc1246646a816970cc3c6e635e3a23f4765a
Skyler Mitchell <skylrmitchll@gmail.com>
2015-01-23 12:27:01 -1000

    added a loading message to the page

--- client/controllers/homeCtrl.js
+++ client/controllers/homeCtrl.js
@@ -9,17 +9,24 @@ controllers.controller('homeCtrl', ['$scope', '$http', function ($scope, $http) 
 	$scope.getCalendar = function () {
 		var id = $scope.calendarId;
 		var url = '/calendars/' + encodeURIComponent(id);
+
+		$scope.loadingFeed = true;
 		
 		$http.get(url)
 		.success(function(data, status, headers, config) {
 			// this callback will be called asynchronously
 			// when the response is available
 			$scope.events = data;
+			$scope.loadingFeed = false;
 		})
 		.error(function(data, status, headers, config) {
 			// called asynchronously if an error occurs
 			// or server returns response with an error status.
-			$scope.events = data + '\n' + status;
+			$scope.error = {
+				data : data,
+				status : status
+			};
+			$scope.loadingFeed = false;
 		});
 	}
 }]);
