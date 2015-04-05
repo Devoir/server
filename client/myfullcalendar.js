05e8a216510e1e4603c06674b46f7e01582cc6c4
Cory <cjkirk09@gmail.com>
2015-04-04 23:54:35 -0600

    can add a second hardcoded course and can remove them

--- client/myfullcalendar.js
+++ client/myfullcalendar.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
 
     // page is now ready, initialize the calendar...
-	
+	var courses= [];
 	
 	 function addCourse() {
 		var eventlist = [];
+		//hardcoded courses for now, only allowing 2 for now
+		if( courses.length == 2)
+			return;
 		var calendarId = 'https://learningsuite.byu.edu/iCalFeed/ical.php?courseID=HD832sKIIdzI';
+		if( courses.length == 1 ){
+			calendarId = 'https://learningsuite.byu.edu/iCalFeed/ical.php?courseID=AJRz4eKuTIIY';
+		}
 		var courseId = 5;
         $.ajax({
 			type:"POST",
@@ -17,25 +23,63 @@ $(document).ready(function() {
 			for (var d in data1){
 				eventlist.push(
 				{	title: data1[d].description,
-					start: data1[d].start,
+					start: data1[d].start_date,
 					allDay: false});
+			}
+			var Color = 'yellow';
+			if( courses.length == 1 ){
+				Color = 'orange';
 			}
 			console.log("course populated");
 			var course = {
-				color: 'yellow',   // an option!
-				textColor: 'black', // an option!
-				events : eventlist
+				color: Color,   
+				textColor: 'black', 
+				
+				events : eventlist,
+				id: courses.length
 			};	
 			$('#calendar').fullCalendar('addEventSource', course);
+			courses.push(course);
+			addCourseToList(course);
+			//var list = $('#calendar').fullCalendar('getEventList');
+			//$('#calendar').fullCalendar('refetchEvents');
+			
 		}});
     }
-     
+    
+    function addCourseToList( course ){
+		//var obj = $('#loadedcoursed');
+		//var str = $('#loadedcourses').innerHTML;
+		var div = document.createElement("DIV");    
+		var id = "course"+course.id;    
+		div.id = id;
+		div.title= course.id;
+		div.className = "course";
+		div.style.backgroundColor = course.color;
+		var s = 'Course '+course.id;
+		var t = document.createTextNode(s); 
+		
+		div.appendChild(t);
+		//var s = '<div id="course'+course.id+'" style="background-color:'+course.color+'"> Course '+course.id+'</div><br>';
+		//str += s;
+		$('#loadedcourses').append(div);
+	}
+	
+    $('#loadedcourses').click( function(event){
+		
+		//REMOVES A COURSE, NEEDS TO BE ABLE TO TOGGLE THE COURSE
+		console.log("clicked course "+event.target.title);
+		var id = parseInt(event.target.title);
+		var course = courses[id]; 
+		$('#calendar').fullCalendar('removeEventSource',course);
+	 });
      
     $('#calendar').fullCalendar({
         // put your options and callbacks here
         
         
-        eventSources:  []   
+        eventSources:  []
+        //getEventList: function (){ return eventSources;}
     });
    
 	$('#addbutton').click(function(){
